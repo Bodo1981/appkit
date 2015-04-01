8d55a186b4f942f5d144bca4dd41be19236bacbf
Christian Bahl <christian.bahl@gmail.com>
2015-04-01 21:22:54 +0200

    documentation and naming convention

--- appkit/src/main/java/com/christianbahl/appkit/adapter/CBBaseAdapterRecyclerView.java
+++ appkit/src/main/java/com/christianbahl/appkit/adapter/CBBaseAdapterRecyclerView.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2015 Christian Bahl
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.christianbahl.appkit.adapter;
 
 import android.content.Context;
@@ -7,7 +22,16 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Created by cbahl on 18.01.15.
+ * A base {@link RecyclerView.Adapter} with a {@link Context}, {@link LayoutInflater} and
+ * a {@link List} of {@link D} which you would like to display.
+ *
+ * <p>
+ * This activity has an additional implementation of {@link #onBindViewHolder(RecyclerView.ViewHolder,
+ * int, int)} which has the view type as parameter.
+ * </p>
+ *
+ * @author Christian Bahl
+ * @see RecyclerView.Adapter
  */
 public abstract class CBBaseAdapterRecyclerView<D> extends RecyclerView.Adapter {
 
@@ -20,18 +44,43 @@ public abstract class CBBaseAdapterRecyclerView<D> extends RecyclerView.Adapter 
     this.inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
   }
 
+  /**
+   * Set the items
+   *
+   * @param items {@link List}
+   */
   public void setItems(List<D> items) {
     this.items = items;
   }
 
+  /**
+   * Get the items
+   *
+   * @return items {@link List}
+   */
   public List<D> getItems() {
     return items;
   }
 
+  /**
+   * Get item at position
+   *
+   * @param position position
+   * @return item at position
+   */
   public D getItem(int position) {
     return items.get(position);
   }
 
+  /**
+   * Add items
+   *
+   * <p>
+   * If {@link #items} is null an empty {@link List} will be created first.
+   * </p>
+   *
+   * @param items {@link List}
+   */
   public void addNewItems(List<D> items) {
     if (this.items == null) {
       this.items = createEmptyList();
@@ -40,6 +89,15 @@ public abstract class CBBaseAdapterRecyclerView<D> extends RecyclerView.Adapter 
     this.items.addAll(0, items);
   }
 
+  /**
+   * Add a single item.
+   *
+   * <p>
+   * If {@link #items} is null an empty {@link List} will be created first.
+   * </p>
+   *
+   * @param item {@link D}
+   */
   public void addNewItem(D item) {
     if (this.items == null) {
       this.items = createEmptyList();
@@ -48,6 +106,11 @@ public abstract class CBBaseAdapterRecyclerView<D> extends RecyclerView.Adapter 
     this.items.add(item);
   }
 
+  /**
+   * Creates an empty {@link List}
+   *
+   * @return empty {@link List}
+   */
   private List<D> createEmptyList() {
     return new ArrayList<>();
   }
