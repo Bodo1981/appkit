7380f1005d09332eeddc46425f4fca673044cd0e
Christian Bahl <christian.bahl@gmail.com>
2015-07-24 20:56:29 +0200

    Remove ignored files

--- sample/src/main/java/com/christianbahl/appkit/sample/MainActivity.java
+++ sample/src/main/java/com/christianbahl/appkit/sample/MainActivity.java
@@ -1,28 +1,11 @@
 package com.christianbahl.appkit.sample;
 
-import com.christianbahl.appkit.core.activity.CBActivityMvpToolbarTabs;
-import com.christianbahl.appkit.core.view.CBMvpView;
-import java.util.List;
+import android.support.v4.app.Fragment;
+import com.christianbahl.appkit.core.activity.CBActivityFragment;
 
-public class MainActivity
-    extends CBActivityMvpToolbarTabs<List<String>, CBMvpView<String>, MainPresenter, MainAdapter>
-    implements CBMvpView<String> {
+public class MainActivity extends CBActivityFragment  {
 
-  @Override protected MainAdapter createAdapter() {
-    return new MainAdapter(getSupportFragmentManager());
-  }
-
-  @Override public MainPresenter createPresenter() {
-    return new MainPresenter();
-  }
-
-  @Override public void setData(List<String> data) {
-    adapter.setTabs(data);
-    adapter.notifyDataSetChanged();
-    tabs.setTabsFromPagerAdapter(adapter);
-  }
-
-  @Override public void loadData(boolean pullToRefresh) {
-    presenter.loadMenu(pullToRefresh);
+  @Override protected Fragment createFragmentToDisplay() {
+    return new MainFragment();
   }
 }
