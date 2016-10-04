aeb7c545f4fc8c1e079013f0dfc17d2056412046
Christian Bahl <christian.bahl@gmail.com>
2016-10-04 09:03:22 +0200

    new Base activities for View State Handling

--- viewstate/src/main/java/com/christianbahl/appkit/viewstate/activity/CBActivityToolbarFragmentViewState.java
+++ viewstate/src/main/java/com/christianbahl/appkit/viewstate/activity/CBActivityToolbarFragmentViewState.java
@@ -1,8 +1,39 @@
 package com.christianbahl.appkit.viewstate.activity;
 
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+
 /**
  * @author Christian Bahl
  */
+public abstract class CBActivityToolbarFragmentViewState extends CBActivityToolbarViewState {
 
-public class CBActivityToolbarFragmentViewState {
+  @Override protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    if (savedInstanceState == null) {
+      Fragment fragment = createFragmentToDisplay();
+
+      if (fragment == null) {
+        throw new NullPointerException(
+            "Fragment is null. Did you return null in createFragmentToDisplay()?");
+      }
+
+      getSupportFragmentManager().beginTransaction()
+          .replace(com.christianbahl.appkit.core.R.id.contentView, fragment)
+          .commit();
+    }
+  }
+
+  @Override @NonNull protected Integer getLayoutRes() {
+    return com.christianbahl.appkit.core.R.layout.cb_activity_toolbar_fragment;
+  }
+
+  /**
+   * Returns the {@link Fragment} which should be displayed by this activity.
+   *
+   * @return {@link Fragment}
+   */
+  @NonNull protected abstract Fragment createFragmentToDisplay();
 }
--- viewstate/src/main/java/com/christianbahl/appkit/viewstate/activity/CBActivityToolbarViewState.java
+++ viewstate/src/main/java/com/christianbahl/appkit/viewstate/activity/CBActivityToolbarViewState.java
@@ -1,8 +1,81 @@
 package com.christianbahl.appkit.viewstate.activity;
 
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.ActionBar;
+import android.support.v7.widget.Toolbar;
+import com.christianbahl.appkit.viewstate.R;
+import com.hannesdorfmann.mosby.mvp.viewstate.MvpViewStateActivity;
+
 /**
  * @author Christian Bahl
  */
+public abstract class CBActivityToolbarViewState extends MvpViewStateActivity {
 
-public class CBActivityToolbarViewState {
+  public Toolbar toolbar;
+
+  @Override protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    if (getIntent() != null && getIntent().getExtras() != null) {
+      readExtras(getIntent().getExtras());
+    }
+
+    Integer layoutRes = getLayoutRes();
+    if (layoutRes == null) {
+      throw new NullPointerException("LayoutRes is null. Did you return null in getLayoutRes()?");
+    }
+    setContentView(layoutRes);
+  }
+
+  @Override public void onContentChanged() {
+    super.onContentChanged();
+
+    toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+    if (toolbar == null) {
+      throw new IllegalStateException("The toolbar is null. "
+          + "You have to provide a View with R.id.toolbar in your inflated xml layout");
+    }
+
+    setSupportActionBar(toolbar);
+
+    ActionBar actionBar = getSupportActionBar();
+    if (actionBar != null) {
+      actionBar.setDisplayShowTitleEnabled(isDisplayShowTitleEnabled());
+    }
+  }
+
+  /**
+   * <p>
+   * Should the title be displayed in the toolbar.
+   * </p>
+   *
+   * @return <code>true</code> if title should be displayed in toolbar otherwise <code>false</code>
+   */
+  protected boolean isDisplayShowTitleEnabled() {
+    return true;
+  }
+
+  /**
+   * <p>
+   * Provide the layout res id for the activity.
+   * </p>
+   *
+   * @return layout res id
+   */
+  @NonNull protected Integer getLayoutRes() {
+    return R.layout.cb_activity_toolbar;
+  }
+
+  /**
+   * <p>
+   * Handle extra bundle data.
+   * </p>
+   *
+   * @param bundle bundle with extras passed to activity
+   */
+  protected void readExtras(@NonNull Bundle bundle) {
+
+  }
 }
