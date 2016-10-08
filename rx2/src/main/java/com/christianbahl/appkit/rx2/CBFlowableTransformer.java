40ee623a3f7a02ac5d004c0360b73da09a0d05c5
Christian Bahl <christian.bahl@gmail.com>
2016-10-08 10:10:48 +0200

    added license to some file Heiders and fixed flowable Transformer

--- rx2/src/main/java/com/christianbahl/appkit/rx2/CBFlowableTransformer.java
+++ rx2/src/main/java/com/christianbahl/appkit/rx2/CBFlowableTransformer.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2015 Christian Bahl
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.christianbahl.appkit.rx2;
 
 import io.reactivex.Flowable;
@@ -21,7 +36,7 @@ public class CBFlowableTransformer<T> implements FlowableTransformer<T, T> {
   public CBFlowableTransformer() {
   }
 
-  @Override public Publisher<? extends T> apply(Flowable<T> flowable) throws Exception {
-    return flowable.subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread());
+  @Override public Publisher<T> apply(Flowable<T> upstream) throws Exception {
+    return upstream.subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread());
   }
 }
